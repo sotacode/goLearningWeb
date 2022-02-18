ca4d7d5d3da5b28a72b00a2750e408f90fb1b16d
SotaCode <sotacodeprivate@gmail.com>
2022-02-18 17:02:54 -0300

    Firsts handlers and error in server added

--- 01-introduccion/goweb.go
+++ 01-introduccion/goweb.go
@@ -6,11 +6,31 @@ import (
 	"net/http"
 )
 
+//handler
+
+func HolaMundo(rw http.ResponseWriter, r *http.Request) {
+	fmt.Println("El metodo utilizado es: ", r.Method)
+	fmt.Fprintln(rw, "Hola Mundo")
+}
+
+func PageNF(rw http.ResponseWriter, r *http.Request) {
+	http.NotFound(rw, r)
+}
+
+func Error(rw http.ResponseWriter, r *http.Request) {
+	http.Error(rw, "Ocurrio un error", http.StatusNotFound)
+}
+
 func main() {
+
+	//Router
+	http.HandleFunc("/", HolaMundo)
+	http.HandleFunc("/page", PageNF)
+	http.HandleFunc("/error", Error)
 
 	//crear servidor
 
 	fmt.Println("El servidor esta corriendo en el puerto 3000")
-	fmt.Println("Run server: http://localhost:3000/")
+	fmt.Println("Run server: http://localhost:3000")
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
